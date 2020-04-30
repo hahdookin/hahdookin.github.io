f470083c7f318d14f871ba2df1af00f17f22e2a2
Christopher Pane <hahdookin@gmail.com>
2020-04-30 14:07:14 -0400

    Auto Commit

--- nav/script.js
+++ nav/script.js
@@ -4,9 +4,4 @@ import * as Utils from "../utils.js";
 // GLOBALS
 const mainTitle = document.getElementById('main-title');
 
-const canvas = document.getElementById('game') || null;
-if (canvas) {
- 
-}
-
 Utils.writeText(mainTitle)
